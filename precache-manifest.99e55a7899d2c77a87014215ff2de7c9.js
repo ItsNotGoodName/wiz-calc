0ca6db276d7d48e4243469c8de1c38f7cd544185
ItsNotGoodName <gurnaindeol@gmail.com>
2020-09-07 21:11:52 -0700

    Updates

---
+++ precache-manifest.99e55a7899d2c77a87014215ff2de7c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3377a494410bf3b4aae2e3799fd8fdd4",
+    "url": "/wiz-calc/index.html"
+  },
+  {
+    "revision": "7289bb24fa3e3febb1c0",
+    "url": "/wiz-calc/static/css/2.ceb79488.chunk.css"
+  },
+  {
+    "revision": "a39308b0689e23b6aa53",
+    "url": "/wiz-calc/static/css/main.5de99c17.chunk.css"
+  },
+  {
+    "revision": "7289bb24fa3e3febb1c0",
+    "url": "/wiz-calc/static/js/2.bad50513.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/wiz-calc/static/js/2.bad50513.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a39308b0689e23b6aa53",
+    "url": "/wiz-calc/static/js/main.6f350bb7.chunk.js"
+  },
+  {
+    "revision": "337366c744f7f59e5fe5",
+    "url": "/wiz-calc/static/js/runtime-main.75186e6d.js"
+  }
+]);
